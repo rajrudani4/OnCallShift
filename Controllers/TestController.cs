9911a60069d0f2e2a164d56323d9083fd34e33c9
rajrudani4 <rajrudani444@gmail.com>
2024-05-21 00:50:55 -0700

    Create TestController.cs

---
+++ Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnCallShift.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Test Controller");
+        }
+    }
+}
